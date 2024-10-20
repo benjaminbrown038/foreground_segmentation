bc00090e59a523c80261d41dbf96bdaa27c97206
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-20 13:01:14 -0400

    Create 18_01_foreground_segmentation.py

---
+++ 18_01_foreground_segmentation.py
@@ -0,0 +1,105 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import matplotlib.pyplot as plt
+
+img 
+bg_img
+bg_img
+
+plt.figure
+plt.subplot
+plt.subplot
+
+mp_selfie_segmenation
+segment 
+
+img
+results
+img
+img_seg_mask
+threshold
+binary_mask
+
+plt.figure
+plt.subplot
+plt.subplot
+plt.subplot
+
+mask3d
+img_out
+
+plt.figure; 
+plt.subplot; plt.axis; plt.imshow; plt.title
+plt.subplot; plt.axis; plt.imshow; plt.title
+plt.subplot
+plt.subplot
+
+
+bg_gray 
+bg_img_gray
+
+img_out
+
+plt.figure
+plt.subplot
+plt.subplot
+plt.subplot
+
+threshold
+title
+img_out
+img_out_bw
+mask3d
+binary_mask
+
+for idx in range:
+  temp
+  title.append
+
+img
+results
+img
+
+bg_gray
+bg_img_gray
+
+for idx in range
+
+
+for idx in range:
+
+def image_seg_enhance():
+
+    return output_image
+
+def imgage_seg_enhance_blur():
+
+    return output_image
+
+img 
+bg_img
+bg_img
+
+thresh
+bg_gray
+bg_rep
+bg_rep_gray
+
+bg_blur
+bg_gray_blur
+bg_rep_blur
+bg_rep_gray_blur
+
+plt.figure
+plt.subplot
+plt.subplot
+plt.subplot
+plt.subplot
+
+plt.subplot
+plt.subplot
+plt.subplot
+plt.subplot
+
+
